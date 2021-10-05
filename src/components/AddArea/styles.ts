f79e3cec56c137da5b202329f23f744c6f7b0ef6
Dieferson de Oliveira <diiieferson@gmail.com>
2021-10-05 19:29:19 -0300

    first commit

---
+++ src/components/AddArea/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div` 
+    border: 1px solid #555;
+    border-radius: 5px;
+    margin: 20px 0;
+    display: flex;
+    align-items: center;
+`
